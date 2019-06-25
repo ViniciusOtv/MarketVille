6220c4ac284349b80ff8a8e7e8c0c40b7a68f5fc
viniciusotv <vinicius.silva@dotzmkt.com.br>
2019-06-25 07:04:25 -0300

    Adicionando controlador dos produtos e respectiva view

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarktVille.DAL;
+using MarktVille.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarktVille.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly DatabaseDb _context;
+
+        public ProductController(DatabaseDb context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new HomeIndexViewModel();
+            model.Products = _context.Products.OrderByDescending(x => x.ProductId).ToArray();
+            return View(model);
+        }
+    }
+}
