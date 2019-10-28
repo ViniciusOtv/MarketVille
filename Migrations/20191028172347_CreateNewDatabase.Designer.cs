2edb91cc32c882f0f0230479b9be8b5a9eb82433
vinicuis.silva <vinicius.silva@dotzmkt.com.br>
2019-10-28 18:18:56 -0300

    Adicionando projeto de cadastro com Angular, e nova atualização da base de dados para o docker

--- Migrations/20191028172347_CreateNewDatabase.Designer.cs
+++ Migrations/20191028172347_CreateNewDatabase.Designer.cs
@@ -4,14 +4,16 @@ using MarktVille.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MarktVille.Migrations
 {
     [DbContext(typeof(DatabaseDb))]
-    partial class DatabaseDbModelSnapshot : ModelSnapshot
+    [Migration("20191028172347_CreateNewDatabase")]
+    partial class CreateNewDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
