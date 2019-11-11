4d898622e0d98725a0047a86c2c73b97ef5a9de9
Vinicius Otávio Soares da Silva <vinicius.silva@dotzmkt.com.br>
2019-11-10 22:27:08 -0300

    Adicionando seletor de imagens do produto na página de produto detalhe

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -39,3 +39,21 @@ $(document).ready(function () {
         ]
     })
 });
+
+
+
+// Change active class on product sizes.
+
+var sizes = jQuery(".product--size").find("span");
+sizes.click(function() {
+  sizes.removeClass("active");
+  $(this).addClass("active");
+});
+
+// Change image on thumbnail click.
+var thumbs = $(".icon-images").find("img");
+thumbs.click(function() {
+  var src = $(this).attr("src");
+  var dp = $(".display-img");
+  dp.attr("src", src);
+});
